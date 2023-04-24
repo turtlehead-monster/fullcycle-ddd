64d014bb037e52be07ce41dac61fc0b9ec54159e
Turtle Head Monster <turtlehead.monster@gmail.com>
2023-04-24 14:23:06 -0300

    repository interface created

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import Address from "./entity/address";
-import Customer from "./entity/customer";
-import Order from "./entity/order";
-import OrderItem from "./entity/order_item";
+import Address from "./domain/entity/address";
+import Customer from "./domain/entity/customer";
+import Order from "./domain/entity/order";
+import OrderItem from "./domain/entity/order_item";
 
 const address = new Address("Rua", 13, "77032145", "São Paulo");
 
